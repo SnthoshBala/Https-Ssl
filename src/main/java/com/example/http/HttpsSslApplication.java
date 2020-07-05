784041c27396faf88647b06b2c3d980337011a43
SnthoshBala <balasb00714@gmail.com>
2020-07-05 14:52:09 +0530

    first commit

---
+++ src/main/java/com/example/http/HttpsSslApplication.java
@@ -0,0 +1,13 @@
+package com.example.http;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsSslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpsSslApplication.class, args);
+	}
+
+}
